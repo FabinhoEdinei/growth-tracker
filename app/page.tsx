dcc584b381e67fd27d8f032792c99d076a40335c
FabinhoEdinei <59501299+FabinhoEdinei@users.noreply.github.com>
2026-03-09 13:04:35 +0000

    feitos ajuste de retorna o app

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+'use client';
+
 import dynamic from 'next/dynamic';
 
 const SoftNeuralField = dynamic(
   () => import('./components/SoftNeuralField/SoftNeuralField'),
-  { 
+  {
     ssr: false,
     loading: () => (
       <div className="flex items-center justify-center h-screen bg-black">
